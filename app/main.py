74f9ac058ae4d7163cb28bce5f00ed18ec6004dd
jabertuhin <jabertuhin@gmail.com>
2022-07-06 00:06:47 +0600

    Read project properties from json file.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
+import json
 from fastapi import FastAPI
 
-from app.route import health
+from app.route import health, string_operations
+from app import project_root_path
 
-# TODO: Read api_version and title from config file.
-api_version = "v0.1"
-title = "DVD Rental"
-app = FastAPI(title=title, version=api_version)
+
+project_properties = json.loads(
+    (project_root_path / "project_properties.json").read_text()
+)
+
+
+app = FastAPI(
+    title=project_properties["project_title"], version=project_properties["api_version"]
+)
 
 
 # including router in the app
 app.include_router(health.router)
+app.include_router(string_operations.router)
